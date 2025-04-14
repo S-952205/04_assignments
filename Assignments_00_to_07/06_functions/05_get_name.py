739d97a2829272c295bd6300597d9cce0de6a85a
Syed Sufyan <sufyansyed157@gmail.com>
2025-04-15 02:47:33 +0500

    5th porgram added

---
+++ Assignments_00_to_07/06_functions/05_get_name.py
@@ -0,0 +1,15 @@
+# iss program main hum function banainge jo hamara name string main return kray ga.  
+
+def get_name():
+    
+    return 'Sufyan'
+
+
+def main():
+    
+    name: str = get_name()
+    print(f'Howdy {name} ! 🤠')
+
+
+if __name__ == '__main__':
+    main()
